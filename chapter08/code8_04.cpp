ad5a3767570c00526036dba829d8367d5df00c88
hugo-maker <hibiki.for.business1@gmail.com>
2021-07-16 16:33:55 +0900

    Added today's activities

---
+++ chapter08/code8_04.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+struct Node
+{
+  Node * next;
+  string name;
+
+  Node(string name_ = "") : next(NULL), name(name_) {}
+};
+
+Node * nil;
+
+void init()
+{
+  nil = new Node();
+  nil->next = nil;
+}
+
+void print_list()
+{
+  Node * cur = nil->next;
+  for (; cur != nil; cur = cur->next)
+  {
+    cout << cur->name << " -> ";
+  }
+  cout << endl;
+}
+
+void insert(Node * v, Node * p = nil)
+{
+  v->next = p->next;
+  p->next = v;
+}
+
+int main()
+{
+  init();
+
+  vector<string> names = {"yamamoto",
+                          "watanabe",
+                          "ito",
+                          "takahashi",
+                          "suzuki",
+                          "sato"};
+
+  for (int i = 0; i < (int)names.size(); ++i)
+  {
+    Node * node = new Node(names[i]);
+
+    insert(node);
+
+    cout << "step" << i << ": ";
+    print_list();
+  }
+
+  return 0;
+}
